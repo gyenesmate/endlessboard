7f24b94ef24e476d394fef84d3c96b5335dc0ee3
gyenesmate <gyenes.mate2002@gmail.com>
2025-12-14 14:26:18 +0100

    First auto deploy test

--- endless-board-app/src/app/app.module.ts
+++ endless-board-app/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     RouterModule.forRoot(routes),
     AngularFireModule.initializeApp(environment.firebase),
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      enabled: !isDevMode(), //!isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
